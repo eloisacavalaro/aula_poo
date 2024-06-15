a88dd00bb1b751cf80b65b3cdcd6630f0b17dd0b
Eloisa Cavalaro <eloisa.cavalaro@gmail.com>
2024-06-14 21:26:21 -0300

    Exercício 4 resolvido

--- src/main/java/com/poo/introducao/Lista1.java
+++ src/main/java/com/poo/introducao/Lista1.java
@@ -4,18 +4,20 @@ import java.util.Scanner;
 
 public class Lista1 {
 
-    public static void ex3(){
+    public static void ex4(){
 
-        Scanner scanner = new Scanner(System.in);
-		double valorDigitadoEmReais;
-		double valorConvertidoParaDolar;
+		Scanner scanner = new Scanner(System.in);
+		int numeroDigitado;
+    	int antecessor;
+    	int sucessor;
 
-		System.out.print("Digite o valor em reais: R$ ");
-        valorDigitadoEmReais = scanner.nextDouble();
-		
-		valorConvertidoParaDolar = valorDigitadoEmReais / 5.25;
+		System.out.println("Digite um número para saber seu antecessor e sucessor:");
+		numeroDigitado = scanner.nextInt();
 
-		System.out.printf("O valor em dólares é: " + valorConvertidoParaDolar);
+		antecessor = numeroDigitado - 1;
+		sucessor = numeroDigitado + 1;
+
+		System.out.println("O sucessor de "+ numeroDigitado + " é " + sucessor+ " e seu antecessor é "+ antecessor +".");
 
     }
         
