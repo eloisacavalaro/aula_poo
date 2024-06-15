6cd058cd4b497981d81c8617a52262d1365fd104
Eloisa Cavalaro <eloisa.cavalaro@gmail.com>
2024-06-14 22:04:50 -0300

    Versão final

--- src/main/java/com/poo/introducao/IntroducaoApplication.java
+++ src/main/java/com/poo/introducao/IntroducaoApplication.java
@@ -11,8 +11,14 @@ public class IntroducaoApplication {
 		SpringApplication.run(IntroducaoApplication.class, args);
 
 		Lista1 lista = new Lista1();
+		lista.ex1();
+		lista.ex2();
+		lista.ex3();
+		lista.ex4();
+		lista.ex5();
+		lista.ex6();
+		lista.ex7();
 		lista.ex8();
-
 		
 	}
 
--- src/main/java/com/poo/introducao/Lista1.java
+++ src/main/java/com/poo/introducao/Lista1.java
@@ -4,7 +4,126 @@ import java.util.Scanner;
 
 public class Lista1 {
 
-    public static void ex8(){
+	public static void ex1(){
+
+        Scanner scanner = new Scanner(System.in);
+		int numero1;
+		int numero2;
+		int resultado;
+
+		System.out.println("Digite um número: ");
+		numero1 = scanner.nextInt();
+
+		System.out.println("Digite outro número: ");
+		numero2 = scanner.nextInt();
+
+        resultado = numero1 + numero2;
+		System.out.println("A soma dos dois numeros é: " + resultado);
+    }
+	public static void ex2(){
+
+        Scanner scanner = new Scanner(System.in);
+		String nome; 
+		String sobrenome;
+
+		System.out.println("Qual é o seu nome?: ");
+		nome = scanner.nextLine();
+
+		System.out.println("Qual é o seu sobrenome?: ");
+		sobrenome = scanner.nextLine();
+
+		System.out.println("Olá " + nome + " "+ sobrenome + "!");
+
+    }
+	public static void ex3(){
+
+        Scanner scanner = new Scanner(System.in);
+		double valorDigitadoEmReais;
+		double valorConvertidoParaDolar;
+
+		System.out.print("Digite o valor em reais: R$ ");
+        valorDigitadoEmReais = scanner.nextDouble();
+		
+		valorConvertidoParaDolar = valorDigitadoEmReais / 5.25;
+
+		System.out.printf("O valor em dólares é: " + valorConvertidoParaDolar);
+
+    } 
+    public static void ex4(){
+
+		Scanner scanner = new Scanner(System.in);
+		int numeroDigitado;
+    	int antecessor;
+    	int sucessor;
+
+		System.out.println("Digite um número para saber seu antecessor e sucessor:");
+		numeroDigitado = scanner.nextInt();
+
+		antecessor = numeroDigitado - 1;
+		sucessor = numeroDigitado + 1;
+
+		System.out.println("O sucessor de "+ numeroDigitado + " é " + sucessor+ " e seu antecessor é "+ antecessor +".");
+
+    }
+	public static void ex5(){
+
+		Scanner scanner = new Scanner(System.in);
+		int larguraTerreno;
+		int comprimentoTerreno;
+		int area;
+		int valorDoM2;
+
+		System.out.println("Qual a largura do terreno em metros?");
+		larguraTerreno = scanner.nextInt();
+
+		System.out.println("Qual a comprimento do terreno em metros?");
+		comprimentoTerreno = scanner.nextInt();
+
+		area = larguraTerreno * comprimentoTerreno;
+
+		System.out.println("Qual o valor do m2 na região em R$?");
+		valorDoM2 = scanner.nextInt();
+
+		System.out.println("O valor total do terreno é: R$" + (valorDoM2 * area) );
+
+    }
+	public static void ex6(){
+
+		Scanner scanner = new Scanner(System.in);
+		double nota1, nota2, nota3, nota4, media;
+
+		System.out.println("Digite sua nota 1:");
+		nota1 = scanner.nextDouble();
+
+		System.out.println("Digete sua nota 2: ");
+		nota2 = scanner.nextDouble();
+
+		System.out.println("Digite sua nota 3: ");
+		nota3 = scanner.nextDouble();
+
+		System.out.println("Digite sua nota 4: ");
+		nota4 = scanner.nextDouble();
+
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+		System.out.println("A média do aluno foi:"+ media);
+
+    }
+	public static void ex7(){
+
+		Scanner scanner = new Scanner(System.in);
+		double temperaturaEmFahrenheit;
+   		double temperaturaEmCelsius;
+
+		System.out.println("Digite a temperatura em Fahrenheit: ");
+		temperaturaEmFahrenheit = scanner.nextDouble();
+
+		temperaturaEmCelsius = (temperaturaEmFahrenheit - 32) / 1.8;
+
+		System.out.println(+temperaturaEmFahrenheit+ " oF corresponde a "+ temperaturaEmCelsius+ " oC");
+
+    }
+	public static void ex8(){
 
 		Scanner scanner = new Scanner(System.in);
 		double distanciaPercorrida;
@@ -21,7 +140,5 @@ public class Lista1 {
 
 		System.out.printf("O consumo médio do veículo é: " + consumoMedio);
 
-    }
-        
-        
+    }    
 }
