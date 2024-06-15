9f6c67960639d7c4c54e3b096f33b200918aba37
Eloisa Cavalaro <eloisa.cavalaro@gmail.com>
2024-06-14 21:24:27 -0300

    Exercício 3 resolvido

--- src/main/java/com/poo/introducao/Lista1.java
+++ src/main/java/com/poo/introducao/Lista1.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 
 public class Lista1 {
 
-    public static void ex2(){
+    public static void ex3(){
 
         Scanner scanner = new Scanner(System.in);
-		String nome; 
-		String sobrenome;
+		double valorDigitadoEmReais;
+		double valorConvertidoParaDolar;
 
-		System.out.println("Qual é o seu nome?: ");
-		nome = scanner.nextLine();
+		System.out.print("Digite o valor em reais: R$ ");
+        valorDigitadoEmReais = scanner.nextDouble();
+		
+		valorConvertidoParaDolar = valorDigitadoEmReais / 5.25;
 
-		System.out.println("Qual é o seu sobrenome?: ");
-		sobrenome = scanner.nextLine();
-
-		System.out.println("Olá " + nome + " "+ sobrenome + "!");
+		System.out.printf("O valor em dólares é: " + valorConvertidoParaDolar);
 
     }
         
